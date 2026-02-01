9c8735f4aa3f0484d69176ab5167fb7e0deac35a
Duncan Shepherd <duncansheph@proton.me>
2026-02-01 09:41:42 -0800

    more setup

--- Scripts/drag_script.gd
+++ Scripts/drag_script.gd
@@ -1,14 +1,35 @@
 extends StaticBody2D
 
-var isDragging = false
+@export
+var value := 1
 
-signal dragsignal;
+@export
+var labelPath := ^"LabelBox/Label"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var is_dragging = false
 
+func _ready():
+	var l : Label = get_node_or_null(labelPath)
+	if l:
+		l.text = str(value)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta): 
+	if is_dragging:
+		var mousepos = get_viewport().get_mouse_position()
+		self.position = Vector2(mousepos.x, mousepos.y)
+
+func _toggleDraggingOn():
+	is_dragging=true
+
+func _toggleDraggingOff():
+	is_dragging=false
+
+func _on_StaticBody2D_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			_toggleDraggingOn()
+		elif event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
+			_toggleDraggingOff()
+	elif event is InputEventScreenTouch:
+		if event.pressed and event.get_index() == 0:
+			self.position = event.get_position()
