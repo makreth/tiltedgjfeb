4878d4f6cb6c6c79e85a168f14bdf250fe15f99e
Duncan Shepherd <duncansheph@proton.me>
2026-02-01 16:44:35 -0800

    music assets

---
+++ Scripts/Output.gd
@@ -0,0 +1,27 @@
+class_name ConveyorOutput extends ConveyorBelt
+
+@export
+var weights : PackedFloat32Array = [
+	1
+]
+
+@export
+var rootNodePath = ^"/root/Root"
+
+@export
+var NumberItem = preload("res://SavedNodes/number_item.tscn")
+
+var rootNode
+var rng : RandomNumberGenerator
+
+func _ready() -> void:
+	super()
+	timer.timeout.connect(_create_item)
+	rootNode = get_node(rootNodePath)
+	rng = RandomNumberGenerator.new()
+
+func _create_item() -> void:
+	var newItem = NumberItem.instantiate() as Item
+	newItem.global_position = self.global_position
+	newItem.value = rng.rand_weighted(weights) + 1
+	rootNode.add_child(newItem)
