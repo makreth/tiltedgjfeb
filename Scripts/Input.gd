91c755733fd3c8b1e9d3cc102fc688ea14e57d62
Duncan Shepherd <duncansheph@proton.me>
2026-02-01 16:03:51 -0800

    working conveyors

---
+++ Scripts/Input.gd
@@ -0,0 +1,47 @@
+class_name ConveyorInput extends ConveyorBelt
+
+@export
+var targetNumber := 1
+
+@export
+var needed := 1
+
+@export
+var targetLabelPath :=  ^"Score/TargetLabel"
+@export
+var depoLabelPath :=  ^"Score/DepoLabel"
+
+var targetLabel: Label
+var depoLabel: Label
+var open = false
+
+var deposited := 0
+
+func _ready() -> void:
+	super()
+
+	targetLabel = get_node_or_null(targetLabelPath)
+	depoLabel = get_node_or_null(depoLabelPath)
+	facing = Facing.INPUT
+	sprite = get_node_or_null(spritePath)
+
+	draw_labels()
+	sprite.stop()
+
+func draw_labels():
+	targetLabel.text = str(targetNumber)
+	depoLabel.text = str(deposited) + "/" + str(needed)
+
+
+func trigger_input(item : Item):
+	if item.is_dragging:
+		return
+	if item.is_pushed:
+		item.is_pushed = false
+		return
+	sprite.play_backwards()
+	item.queue_free()
+	if item.value == needed and deposited < needed:
+		deposited += 1
+	open = false
+	draw_labels()
