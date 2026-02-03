02046024fc5e21e7406d7c65d0681cd51ad5f963
Jacob Christ <jacob@pontech.com>
2026-02-02 21:06:32 -0800

    Fixed broken start game button due to scene rename.

--- title_screen.gd
+++ title_screen.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://root.tscn")
+	get_tree().change_scene_to_file("res://Scenes/mainLevel.tscn")
 
 func _on_credits_pressed() -> void:
 	#get_tree().change_scene_to_file("res://Assets/Scenes/Credits.tscn")
