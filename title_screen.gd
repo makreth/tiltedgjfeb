cf91b5c8f338f06f7bc70efd6fd65362984cb05c
Jacob Christ <jacob@pontech.com>
2026-02-03 10:27:25 -0800

    Added second level so that Jacob can play with the mechanics without messing with Duncan's work.

--- title_screen.gd
+++ title_screen.gd
@@ -3,6 +3,9 @@ extends Node2D
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/mainLevel.tscn")
 
+func _on_start_game_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/jacobLevel.tscn")
+
 func _on_credits_pressed() -> void:
 	#get_tree().change_scene_to_file("res://Assets/Scenes/Credits.tscn")
 	pass
