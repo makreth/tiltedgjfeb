155fc267363eff04c15e95126d4e988825781fe3
Duncan Shepherd <duncansheph@proton.me>
2026-02-04 00:27:54 -0800

    removed unneeded deps

--- Scripts/itemMachine.gd
+++ Scripts/itemMachine.gd
@@ -1,13 +1,9 @@
 class_name ItemMachine extends Node2D
 
 @export
+var operation: Operation.TYPE
+@export
 var facing: Facing.CARDINAL = Facing.CARDINAL.UP
-
-var _itemClass = preload("res://Packed/Item.tscn")
-
-@onready
-var _sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	self.rotation = Facing.radian(facing)
-	_sprite.play()
