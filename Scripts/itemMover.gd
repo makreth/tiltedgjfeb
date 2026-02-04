8bc1fb6523d0ee5760348e56182af12f8c7786f4
Duncan Shepherd <duncansheph@proton.me>
2026-02-04 00:28:09 -0800

    moved calculation to its own class

--- Scripts/itemMover.gd
+++ Scripts/itemMover.gd
@@ -2,29 +2,15 @@ class_name ItemMover extends Area2D
 
 var _outputTarget: ItemReceiver
 var _outputDir: Facing.CARDINAL
-var _linkedReceivers: Array[ItemReceiver]
+var _inputReceiver: ItemReceiver
 var _shift: int
-var _transformer: Callable = _no_op_transformer
 
-func initialize(outputDir: Facing.CARDINAL, linkedReceivers: Array[ItemReceiver], shift: int = 64 ) -> void:
+func initialize(outputDir: Facing.CARDINAL, inputReceiver: ItemReceiver, shift: int = 64 ) -> void:
 	_outputDir = outputDir
 	_shift = shift
-	_linkedReceivers = linkedReceivers
+	_inputReceiver = inputReceiver
 	ORCHESTRATOR.registerPoll(poll, 2)
 	ORCHESTRATOR.registerExecute(pushItem, 2)
-
-## Set the transform function to apply to Items received by the linked ItemReceivers:
-## 
-## callback(Array[Items]) -> Item
-## 
-## The transform function will apply to the Items, then the items are destroyed if the result
-## item is different. If the result item is the same, the Item is preserved.
-## The resulting item is then pushed to the outputTarget: ItemReceiver.
-func setTransformFunction(callback: Callable) -> void:
-	_transformer = callback
-
-static func _no_op_transformer(items: Array[Item]) -> Item:
-	return items[0]
 
 func poll() -> void:
 	_outputTarget = pollForTarget()
@@ -49,37 +35,18 @@ func pollForTarget() -> ItemReceiver:
 		push_warning("Multiple receivers in one square at position: " + str(query.position))
 
 	return hits[0].collider
-
-func _prepareItemsForPush() -> Array[Item]:
-	var result: Array[Item] = []
-	for receiver in _linkedReceivers:
-		var currItem = receiver.getItem()
-		if currItem and not currItem.isLocked():
-			result.append(currItem)
-	return result
-
-static func deleteItems(items: Array[Item]) -> void:
-	for i in range(items.size()):
-		items[i].queue_free()
 		
 func pushItem() -> void:
 	if not _outputTarget:
 		return
-	var items = _prepareItemsForPush()
-	if items.size() != _linkedReceivers.size():
-		return
 	if _outputTarget.isLocked():
 		return
-	var resultItem = _transformer.call(items)
-	if items.size() > 1:
-		deleteItems(items)
-	if items.get(0).value != resultItem.value:
-		deleteItems(items)
-	resultItem.global_position =  Facing.adjacent(_outputDir, global_position, _shift)
+	var item = _inputReceiver.getItem()
+	if not item:
+		return
+	item.global_position = Facing.adjacent(_outputDir, global_position, _shift)
 	
 func isLocked() -> bool:
-	if _prepareItemsForPush().size() != _linkedReceivers.size():
-		return true
 	if not _outputTarget:
 		return true
 	return _outputTarget.isLocked()
