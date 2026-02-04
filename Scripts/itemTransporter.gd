70bc78295f506b6beb93ea15acaa7aa6647fec1e
Duncan Shepherd <duncansheph@proton.me>
2026-02-04 00:29:20 -0800

    update transporter to allow external init

--- Scripts/itemTransporter.gd
+++ Scripts/itemTransporter.gd
@@ -1,4 +1,4 @@
-class_name ItemTrasporter extends ItemMachine
+class_name ItemTransporter extends ItemMachine
 
 @onready
 var _mover: ItemMover = $ItemMover as ItemMover
@@ -7,5 +7,15 @@ var _receiver: ItemReceiver = $ItemReceiver as ItemReceiver
 
 func _ready() -> void:
 	super()
-	_mover.initialize(facing, [_receiver])
+	_initializeChildren()
+
+func _initializeChildren() -> void:
+	_mover.initialize(facing, _receiver)
 	_receiver.initialize(Facing.allExcept(Facing.opposite(facing)), _mover)
+
+func initializeWithNewFacing(newFacing: Facing.CARDINAL) -> void:
+	facing = newFacing
+	_initializeChildren()
+
+func getReceiver() -> ItemReceiver:
+	return _receiver
